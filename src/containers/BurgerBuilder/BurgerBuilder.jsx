373c388a8ff327bc42b8e1861a22578f4224ef0c
Hung Huynh <hungthuynh@gmail.com>
2021-01-14 14:34:02 -0800

    Added navigation items

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls"
 import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -18,7 +19,8 @@ class BurgerBuilder extends Component{
             meat: 0
         },
         totalPrice : 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState (ingredients) {
@@ -62,7 +64,17 @@ class BurgerBuilder extends Component{
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
         this.updatePurchaseState(updatedIngredients);
     }
+    purchaseHandler = () => {
+        this.setState({purchasing:true});
+    };
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
+    purchaseContinueHandler = () => {
+        alert("You continue!");
+    }
     render(){
         const disabledInfo = {
             ...this.state.ingredients
@@ -72,13 +84,19 @@ class BurgerBuilder extends Component{
         }
         return(
            <Fragment>
-               <Modal />
+               <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                   <OrderSummary ingredients={this.state.ingredients}
+                   price={this.state.totalPrice}
+                   purchaseCancelled={this.purchaseCancelHandler}
+                   purchaseContinued={this.purchaseContinueHandler}/>
+               </Modal>
                <Burger ingredients={this.state.ingredients}/>
                <BuildControls 
                ingredientAdded= {this.addIngredientHandler}
                ingredientRemoved={this.removeIngredientsHandler} 
                disabled={disabledInfo}
                purchasable={this.state.purchasable}
+               ordered={this.purchaseHandler}
                price={this.state.totalPrice}/>
            </Fragment> 
         );
